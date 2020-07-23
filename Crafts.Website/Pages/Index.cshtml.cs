70543f8eeb89101f6f914b93c501faa4dac124fb
TonyJimenezDev <TonyJimenezDev@outlook.com>
2020-07-23 00:38:47 -0500

    Add project files.

---
+++ Crafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crafts.Website.Models;
+using Crafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Crafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileProductsService _productService;
+
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productsService)
+        {
+            _logger = logger;
+            _productService = productsService;
+        }
+
+        public void OnGet()
+        {
+            Products = _productService.GetProducts();
+        }
+    }
+}
---
+++ Crafts.Website/Services/JsonFileProductsService.cs
@@ -0,0 +1,29 @@
+﻿using Crafts.Website.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Crafts.Website.Services
+{
+    public class JsonFileProductsService
+    {
+        public IWebHostEnvironment _webHostEnviroment { get; }
+
+        public JsonFileProductsService(IWebHostEnvironment webHostEnviroment) => _webHostEnviroment = webHostEnviroment;
+
+        private string JsonFileName => Path.Combine(_webHostEnviroment.WebRootPath, "data", "products.json"); // Path wwwroot/Folder/Json
+
+        public IEnumerable<Product> GetProducts()
+        {
+            // De-serialize the json text
+            using StreamReader jsonFileReader = File.OpenText(JsonFileName);
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }); 
+        }
+    }
+}
